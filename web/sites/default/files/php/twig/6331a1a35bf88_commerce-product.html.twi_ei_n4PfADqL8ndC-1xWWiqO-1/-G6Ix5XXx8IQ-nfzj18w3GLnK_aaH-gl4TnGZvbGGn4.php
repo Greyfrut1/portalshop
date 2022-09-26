deabb76603fa2674ddc0659db04ef66c6d0032a1
Greyfrut1 <vladchumachenko496@gmail.com>
2022-09-26 17:39:14 +0300

    Update

---
+++ web/sites/default/files/php/twig/6331a1a35bf88_commerce-product.html.twi_ei_n4PfADqL8ndC-1xWWiqO-1/-G6Ix5XXx8IQ-nfzj18w3GLnK_aaH-gl4TnGZvbGGn4.php
@@ -0,0 +1,108 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/custom/ps_blog/templates/commerce/commerce-product.html.twig */
+class __TwigTemplate_36505e6c00266f9d097232e9f67810a0 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 27
+        $context["classes"] = [0 => "commerce", 1 => ("commerce--" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(        // line 29
+($context["product_type"] ?? null), 29, $this->source))), 2 => ((("commerce--" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(        // line 30
+($context["product_type"] ?? null), 30, $this->source))) . "-") . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(($context["view_mode"] ?? null), 30, $this->source))), 3 => ((        // line 31
+($context["view_mode"] ?? null)) ? (("ncommerce--view-mode-" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(($context["view_mode"] ?? null), 31, $this->source)))) : (""))];
+        // line 34
+        echo "<article";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null)], "method", false, false, true, 34), 34, $this->source), "html", null, true);
+        echo ">
+  ";
+        // line 35
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title_prefix"] ?? null), 35, $this->source), "html", null, true);
+        echo "
+  ";
+        // line 36
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title_suffix"] ?? null), 36, $this->source), "html", null, true);
+        // line 37
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->withoutFilter($this->sandbox->ensureToStringAllowed(($context["product"] ?? null), 37, $this->source), "variation_attributes"), "html", null, true);
+        // line 38
+        echo "</article>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/ps_blog/templates/commerce/commerce-product.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  57 => 38,  55 => 37,  53 => 36,  49 => 35,  44 => 34,  42 => 31,  41 => 30,  40 => 29,  39 => 27,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/custom/ps_blog/templates/commerce/commerce-product.html.twig", "/var/www/web/themes/custom/ps_blog/templates/commerce/commerce-product.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("set" => 27);
+        static $filters = array("clean_class" => 29, "escape" => 34, "without" => 37);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set'],
+                ['clean_class', 'escape', 'without'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
